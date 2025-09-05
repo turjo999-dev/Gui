eb6192a6876a9362267aca653c38458d9ba94d9c
yeamin-lang <samadyeamin@gmail.com>
2025-09-05 10:40:17 +0600

    Updated TransactionHistoryGui.java

--- src/main/java/dev/turjo/easyshopgui/gui/TransactionHistoryGui.java
+++ src/main/java/dev/turjo/easyshopgui/gui/TransactionHistoryGui.java
@@ -13,6 +13,7 @@ import org.bukkit.inventory.ItemStack;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
